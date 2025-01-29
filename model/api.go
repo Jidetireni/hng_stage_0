10e2abf668ad5aeef7627f153ddebe6b13e14854
Jide Tireni <jtirenipraise@gmail.com>
2025-01-29 16:26:32 +0100

    add local time zone

--- model/api.go
+++ model/api.go
@@ -1,6 +1,9 @@
 package model
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 type ApiResponse struct {
 	Email           string `json:"email"`
@@ -9,9 +12,26 @@ type ApiResponse struct {
 }
 
 func NewApiResponse() (*ApiResponse, error) {
+
+	location, err := timezone()
+	if err != nil {
+		return nil, err
+	}
+
+	currentTime := time.Now().In(location)
 	return &ApiResponse{
 		Email:           "jtirenipraise@gmail.com",
-		CurrentDateTime: time.Now().UTC().Format(time.RFC3339),
+		CurrentDateTime: currentTime.Format(time.RFC3339),
 		GithubUrl:       "https://github.com/Jidetireni/hng_stage_0.git",
 	}, nil
+}
+
+func timezone() (*time.Location, error) {
+	location, err := time.LoadLocation("Africa/Lagos")
+	if err != nil {
+		return nil, fmt.Errorf("could not load location: %v", err)
+	}
+
+	return location, nil
+
 }
