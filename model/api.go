ef28537ffb9be54c2ad72a84f48305c8ee6b332a
Jide Tireni <jtirenipraise@gmail.com>
2025-01-29 16:52:39 +0100

    api response struct

--- model/api.go
+++ model/api.go
@@ -1,7 +1,6 @@
 package model
 
 import (
-	"fmt"
 	"time"
 )
 
@@ -12,26 +11,9 @@ type ApiResponse struct {
 }
 
 func NewApiResponse() (*ApiResponse, error) {
-
-	location, err := timezone()
-	if err != nil {
-		return nil, err
-	}
-
-	currentTime := time.Now().In(location)
 	return &ApiResponse{
 		Email:           "jtirenipraise@gmail.com",
-		CurrentDateTime: currentTime.Format(time.RFC3339),
+		CurrentDateTime: time.Now().UTC().Format(time.RFC3339),
 		GithubUrl:       "https://github.com/Jidetireni/hng_stage_0.git",
 	}, nil
-}
-
-func timezone() (*time.Location, error) {
-	location, err := time.LoadLocation("Africa/Lagos")
-	if err != nil {
-		return nil, fmt.Errorf("could not load location: %v", err)
-	}
-
-	return location, nil
-
 }
