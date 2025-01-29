97531bd56eff703aa86f71d7de49170f17065be7
Jide Tireni <jtirenipraise@gmail.com>
2025-01-29 15:13:42 +0100

    rounte handler

---
+++ route/router.go
@@ -0,0 +1,21 @@
+package route
+
+import (
+	"hng_stage_0/model"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func getApiResponse(context *gin.Context) {
+	response, err := model.NewApiResponse()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch response. Try again later."})
+		return
+	}
+	context.JSON(http.StatusOK, response)
+}
+
+func RegisterRoutes(router *gin.Engine) {
+	router.GET("/hngstage0", getApiResponse)
+}
